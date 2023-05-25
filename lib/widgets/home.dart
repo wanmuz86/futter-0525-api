a2b5a351df33a396ab3b5bb3657c0160f6b7cbd2
Wan Muzaffar <wanmuz86@gmail.com>
2023-05-25 16:39:54 +0800

    first commit

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -13,39 +13,10 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   var movies = [
-    // {
-    //   "Title": "The Lord of the Rings: The Fellowship of the Ring",
-    //   "Year": "2001",
-    //   "imdbID": "tt0120737",
-    //   "Type": "movie",
-    //   "Poster":
-    //       "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg"
-    // },
-    // {
-    //   "Title": "The Lord of the Rings: The Return of the King",
-    //   "Year": "2003",
-    //   "imdbID": "tt0167260",
-    //   "Type": "movie",
-    //   "Poster":
-    //       "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
-    // },
-    // {
-    //   "Title": "The Lord of the Rings: The Two Towers",
-    //   "Year": "2002",
-    //   "imdbID": "tt0167261",
-    //   "Type": "movie",
-    //   "Poster":
-    //       "https://m.media-amazon.com/images/M/MV5BZGMxZTdjZmYtMmE2Ni00ZTdkLWI5NTgtNjlmMjBiNzU2MmI5XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg"
-    // },
-    // {
-    //   "Title": "The Lord of the Rings: The Rings of Power",
-    //   "Year": "2022–",
-    //   "imdbID": "tt7631058",
-    //   "Type": "series",
-    //   "Poster":
-    //       "https://m.media-amazon.com/images/M/MV5BNTg3NjcxYzgtYjljNC00Y2I2LWE3YmMtOTliZTkwYTE1MmZiXkEyXkFqcGdeQXVyNTY4NDc5MDE@._V1_SX300.jpg"
-    // }
+
   ];
+
+  var searchTextEditingController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -57,6 +28,7 @@ class _HomePageState extends State<HomePage> {
               children: [
                 Expanded(
                   child: TextField(
+                    controller: searchTextEditingController,
                     decoration:
                         InputDecoration(hintText: "Enter movie to search"),
                   ),
@@ -70,7 +42,7 @@ class _HomePageState extends State<HomePage> {
                       backgroundColor: MaterialStatePropertyAll(Colors.red),
                     ),
                     onPressed: () {
-                      fetchMovieSearch().then((value) => {
+                      fetchMovieSearch(searchTextEditingController.text).then((value) => {
                         setState((){
                           movies=value;
                         })
@@ -99,9 +71,9 @@ class _HomePageState extends State<HomePage> {
   }
 // What is the data type ? => MovieSearch
   // Is it a List or object => List<MovieSearch> (Data type if have List)
-  Future<List<MovieSearch>> fetchMovieSearch() async {
+  Future<List<MovieSearch>> fetchMovieSearch(searchText) async {
     final response = await http
-        .get(Uri.parse('https://www.omdbapi.com/?s=Lord&apikey=87d10179'));
+        .get(Uri.parse('https://www.omdbapi.com/?s=$searchText&apikey=87d10179'));
 
     if (response.statusCode == 200) {
       // If the server did return a 200 OK response,
